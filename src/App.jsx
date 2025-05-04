e92dd14f4c7ccbd84e09ad0c999b940d9f3b6044
Icaro Jordano <Icarojordano>
2025-05-04 18:42:55 -0300

    version mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SelectMulti from "./components/SelectMulti";
 function App() {
   return (
     <>
-      <div className="w-4/12  pt-32 mx-auto flex" >
+      <div className="lg:w-4/12 px-1 pt-32 mx-auto flex" >
         <SelectMulti/>
       </div>
     </>
